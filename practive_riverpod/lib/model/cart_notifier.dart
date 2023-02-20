9c2346444853c8194dccac06abc17f76cf98ef4c
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2023-02-21 01:18:00 +0900

    Add cart notifier

---
+++ practive_riverpod/lib/model/cart_notifier.dart
@@ -0,0 +1,46 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'cart_notifier.freezed.dart';
+part 'cart_notifier.g.dart';
+
+@riverpod
+class Cart extends _$Cart {
+  @override
+  CartState build() => CartState();
+
+  void add(String id) => state = state.added(id);
+  void delete(String id) => state = state.deleted(id);
+}
+
+@freezed
+class CartState with _$CartState {
+  factory CartState([@Default(<String, int>{}) Map<String, int>map]) = _CartState;
+  CartState._();
+
+  CartState added(String id) {
+    return CartState({
+      ...map,
+      id: (map[id] ?? 0) + 1,
+    });
+  }
+
+  CartState deleted(String id) {
+    return CartState(
+    {
+      ...map,
+      id: map[id]! - 1,
+    }..removeWhere((key, quantity) => quantity <= 0),
+    );
+  }
+
+  late final itemIds = map.keys.toList()..sort((a, b) => a.compareTo(b));
+  late final totalQuantity = map.values.fold<int>(
+    0,
+  (sum, quantity) {
+      return sum + quantity;
+    },
+  );
+  late final isEmpty = totalQuantity <= 0;
+  int quantity(String id) => map[id] ?? 0;
+}
---
+++ practive_riverpod/lib/model/cart_notifier.freezed.dart
@@ -0,0 +1,140 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'cart_notifier.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$CartState {
+  Map<String, int> get map => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $CartStateCopyWith<CartState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $CartStateCopyWith<$Res> {
+  factory $CartStateCopyWith(CartState value, $Res Function(CartState) then) =
+      _$CartStateCopyWithImpl<$Res, CartState>;
+  @useResult
+  $Res call({Map<String, int> map});
+}
+
+/// @nodoc
+class _$CartStateCopyWithImpl<$Res, $Val extends CartState>
+    implements $CartStateCopyWith<$Res> {
+  _$CartStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? map = null,
+  }) {
+    return _then(_value.copyWith(
+      map: null == map
+          ? _value.map
+          : map // ignore: cast_nullable_to_non_nullable
+              as Map<String, int>,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_CartStateCopyWith<$Res> implements $CartStateCopyWith<$Res> {
+  factory _$$_CartStateCopyWith(
+          _$_CartState value, $Res Function(_$_CartState) then) =
+      __$$_CartStateCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({Map<String, int> map});
+}
+
+/// @nodoc
+class __$$_CartStateCopyWithImpl<$Res>
+    extends _$CartStateCopyWithImpl<$Res, _$_CartState>
+    implements _$$_CartStateCopyWith<$Res> {
+  __$$_CartStateCopyWithImpl(
+      _$_CartState _value, $Res Function(_$_CartState) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? map = null,
+  }) {
+    return _then(_$_CartState(
+      null == map
+          ? _value._map
+          : map // ignore: cast_nullable_to_non_nullable
+              as Map<String, int>,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_CartState extends _CartState {
+  _$_CartState([final Map<String, int> map = const <String, int>{}])
+      : _map = map,
+        super._();
+
+  final Map<String, int> _map;
+  @override
+  @JsonKey()
+  Map<String, int> get map {
+    if (_map is EqualUnmodifiableMapView) return _map;
+    // ignore: implicit_dynamic_type
+    return EqualUnmodifiableMapView(_map);
+  }
+
+  @override
+  String toString() {
+    return 'CartState(map: $map)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_CartState &&
+            const DeepCollectionEquality().equals(other._map, _map));
+  }
+
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, const DeepCollectionEquality().hash(_map));
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_CartStateCopyWith<_$_CartState> get copyWith =>
+      __$$_CartStateCopyWithImpl<_$_CartState>(this, _$identity);
+}
+
+abstract class _CartState extends CartState {
+  factory _CartState([final Map<String, int> map]) = _$_CartState;
+  _CartState._() : super._();
+
+  @override
+  Map<String, int> get map;
+  @override
+  @JsonKey(ignore: true)
+  _$$_CartStateCopyWith<_$_CartState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
