06011f52405c75f995edcbdba7c3836e665efcb7
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2023-02-24 22:03:25 +0900

    Add router

---
+++ practice_riverpod/lib/router.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
+import 'package:go_router/go_router.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:practice_riverpod/pages/cart_page/cart_page.dart';
+import 'package:practice_riverpod/pages/items_page/items_page.dart';
+
+part 'router.g.dart';
+
+@riverpod
+GoRouter router(RouterRef ref) => GoRouter(
+  routes: $appRoutes,
+  debugLogDiagnostics: kDebugMode,
+);
+
+@TypedGoRoute<ItemsRoute>(
+  path: '/',
+  routes: [
+  TypedGoRoute<CartRoute>(path: 'cart'),
+],
+)
+
+class ItemsRoute extends GoRouteData {
+  const ItemsRoute();
+  @override
+  Widget build(BuildContext context, GoRouterState state) => const ItemsPage();
+}
+
+
---
+++ practice_riverpod/lib/router.g.dart
@@ -0,0 +1,72 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'router.dart';
+
+// **************************************************************************
+// GoRouterGenerator
+// **************************************************************************
+
+List<GoRoute> get $appRoutes => [
+      $itemsRoute,
+    ];
+
+GoRoute get $itemsRoute => GoRouteData.$route(
+      path: '/',
+      factory: $ItemsRouteExtension._fromState,
+      routes: [
+        GoRouteData.$route(
+          path: 'cart',
+          factory: $CartRouteExtension._fromState,
+        ),
+      ],
+    );
+
+extension $ItemsRouteExtension on ItemsRoute {
+  static ItemsRoute _fromState(GoRouterState state) => const ItemsRoute();
+
+  String get location => GoRouteData.$location(
+        '/',
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location, extra: this);
+}
+
+extension $CartRouteExtension on CartRoute {
+  static CartRoute _fromState(GoRouterState state) => const CartRoute();
+
+  String get location => GoRouteData.$location(
+        '/cart',
+      );
+
+  void go(BuildContext context) => context.go(location, extra: this);
+
+  void push(BuildContext context) => context.push(location, extra: this);
+
+  void pushReplacement(BuildContext context) =>
+      context.pushReplacement(location, extra: this);
+}
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$routerHash() => r'c9ea91de790dd67b3bf2a82562022e49e09905b9';
+
+/// See also [router].
+@ProviderFor(router)
+final routerProvider = AutoDisposeProvider<GoRouter>.internal(
+  router,
+  name: r'routerProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$routerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef RouterRef = AutoDisposeProviderRef<GoRouter>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
